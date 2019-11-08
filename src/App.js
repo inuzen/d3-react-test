4b7c7a8aea3e34846bb58c82e21ed13a426a4799
inuzen <condddr@gmail.com>
2019-11-09 01:34:16 +0300

    added triangle buttons and expansion functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useContext} from 'react';
+import React, {useEffect, useContext, useState, useRef} from 'react';
 import PropTypes from 'prop-types';
 import GlobalState from './context/global/GlobalState';
 import GraphField from './components/GraphFiled';
@@ -7,16 +7,44 @@ import GraphControls from './components/graph-controls/GraphControls';
 
 
 const App = () => {
+  const [expandedRight, setExpandedRight] = useState(false);
+  const [expandedLeft, setExpandedLeft] = useState(false);
+
+  let appWrapperRef = useRef('');
+
+  useEffect(() => {
+
+    expandedLeft ?
+      appWrapperRef.current.style.marginLeft = '0'
+      : appWrapperRef.current.style.marginLeft = '20%';
+
+    expandedRight ?
+      appWrapperRef.current.style.marginRight = '0'
+      : appWrapperRef.current.style.marginRight = '20%';
+
+  },[expandedRight, expandedLeft])
+
+  const onClick = (e) => {
+    e.preventDefault();
+    if(e.target.getAttribute('name')==='expand-left'){
+      setExpandedLeft(!expandedLeft);    
+
+    } else {
+      setExpandedRight(!expandedRight);
+    }
+    e.target.classList.toggle('expand-right');
+
+  }
 
   return (
     <GlobalState>
     <div className='app-container'>
-      <span className='expand expand-left'></span>
-      <div className='app-wrapper flex-row'>
+      <div ref = {appWrapperRef} className='app-wrapper flex-row'>
+        <span name='expand-left' onClick={onClick} className='expand position-left expand-left'></span>
         <GraphField/>
         <GraphControls/>
+        <span name='expand-right' onClick={onClick} className='expand position-right expand-right'></span>
       </div>
-      <span className='expand expand-right'></span>
       </div>
     </GlobalState>
   )
