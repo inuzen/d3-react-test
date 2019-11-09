b32b9f2b10e44f8a0aa129ee1af439d2e7677f76
inuzen <condddr@gmail.com>
2019-11-09 17:31:59 +0300

    Updates

---
+++ precache-manifest.c8d5a09a4e0f43f92fb9cedbbbb3cbab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e593b7080ba9204a4be367562e5f7658",
+    "url": "/d3-react-test/index.html"
+  },
+  {
+    "revision": "1fc75bc12e825e9eab6b",
+    "url": "/d3-react-test/static/css/main.695893ba.chunk.css"
+  },
+  {
+    "revision": "babeff32d6adea62bb53",
+    "url": "/d3-react-test/static/js/2.72313a04.chunk.js"
+  },
+  {
+    "revision": "1fc75bc12e825e9eab6b",
+    "url": "/d3-react-test/static/js/main.f87fd6a9.chunk.js"
+  },
+  {
+    "revision": "d9ea63d7a1a851105091",
+    "url": "/d3-react-test/static/js/runtime-main.0b41d53a.js"
+  }
+]);
